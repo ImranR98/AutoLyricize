fcf72b9e9afd2b364115a8fa60ffc5813df2a80f
rexendevar <46948713+rexendevar@users.noreply.github.com>
2024-12-05 04:23:21 -0700

    Inexact search and persistent Lyricsify support

--- main.py
+++ main.py
@@ -23,6 +23,8 @@ def lyricsify_find_song_lyrics(query):
     If not found, return None.
     """
     # Search Lyricsify for the song using web scraping
+    global inexact
+    inexact = 0
     link = BeautifulSoup(
         requests.get(url="https://www.lyricsify.com/lyrics/" +
                      query.lower().replace(
@@ -36,9 +38,17 @@ def lyricsify_find_song_lyrics(query):
     # If not found, return None
     if divs is None:
         return None
+    #print(str(divs[0]) + "\n\n\n\n")
     # Scrape the song html for the lyrics text
-    song_html='\n'.join(str(divs[0]).split('\n')[1:-1]).replace("<br/>","")
-    return(song_html)
+    try: song_html=str('\n'.join(str(divs[0]).split('\n')[1:-1]).replace("<br/>",""))
+    except:
+        return None
+    #print(song_html + "\n\n\n\n")
+    return(song_html[song_html.find("[ar: "):])
+
+
+
+
 
 
 def genius_find_song_lyrics(query, access_token):
@@ -62,33 +72,44 @@ def genius_find_song_lyrics(query, access_token):
     # If the song has no URL or the artist or song name does not exist in the query, return None
     song = results["response"]["hits"][0]["result"]
     query_lower = query.lower()
+    
+    
+    global inexact
+    inexact = 0
     if song["url"] is None or query_lower.find(song["title"].lower()) < 0 or query_lower.find(song["primary_artist"]["name"].lower()) < 0:
-        return None
+        if requireexact == "y":
+            return None
+        inexact = 1
     # Scrape the song URL for the lyrics text
     page = requests.get(song["url"], headers=headers)
     html = BeautifulSoup(page.text, "html.parser")
     if (html.find("div", id="cloudflare_content")):
         raise Exception(
-            "Scraping encountered Cloudflare and cannot continue.")
+            f"{Color.RED}Scraping encountered Cloudflare and cannot continue.{Color.OFF}")
     target_divs = html.find_all("div", {'data-lyrics-container': "true"})
     lyrics = []
+    
+    
     for div in target_divs:    
         if div is None: # This ususally means the song is an instrumental (exists on the site and was found, but no lyrics)
             return None
         else:
             lyrics = "\n".join("\n".join(div.strings) for div in target_divs).split("\n")
     final_lyrics = "\n".join(lyrics)
-    final_lyrics = final_lyrics.replace("(\n","(").replace("\n)",")").replace(" \n"," ").replace("\n]","]").replace("\n,",",").replace("\n'\n","\n'").replace("\n[","\n\n[") # Removing unwanted line breaks. This mostly works
+    final_lyrics = final_lyrics.replace("(\n","(").replace("\n)",")").replace(" \n"," ").replace("\n "," ").replace("\n]","]").replace("\n,",",").replace("\n'\n","\n'").replace("\n\n[","\n[").replace("\n[","\n\n[") # Removing unwanted line breaks. This mostly works
     if final_lyrics == "":
+        inexact = 0
         return None
     return final_lyrics
+
+
 
 # First, ensure user input exists
 genius_access_token = os.getenv("GENIUS_ACCESS_TOKEN")
 if len(genius_access_token) == 0:
     genius_access_token = None
 if genius_access_token is None:
-    print("Note: The GENIUS_ACCESS_TOKEN environment variable has not been defined. Only Lyricsify.com will be used as a data source.")
+    print(f"{Color.YELLOW}The GENIUS_ACCESS_TOKEN environment variable has not been defined. Genius searches will not be conducted.{Color.OFF}")
 if (len(sys.argv) < 2):
     raise NameError(
         "The song directory path has not been provided as a parameter.")
@@ -105,6 +126,8 @@ open('current.txt', 'a').close()
 try: os.remove('short.txt')
 except OSError: pass
 open('short.txt', 'a').close()
+inexact = 0
+
 
 with open('current.txt', 'a') as current, open('short.txt', 'a') as short:
     total = 0
@@ -113,16 +136,34 @@ with open('current.txt', 'a') as current, open('short.txt', 'a') as short:
             current.write(folder + '/' + file + '\n')
             short.write(file + '\n')
             total += 1
+
+
 if total == 0:
     print("Directory is empty or does not exist.")
 else:
-    overwrite = input("Reply Y to overwrite current lyrics: ").lower()
+    yesno = [ "y", "n" ]
+    overwrite = input("Overwrite current lyrics? y/N ").lower()
+    if overwrite not in yesno:
+        print(f"{Color.YELLOW}Interpreting unknown response as no{Color.OFF}")
+    elif overwrite == "y" and os.getenv("I_WANT_SYNCED_LYRICS") == "True":
+        evenifunsynced = input("Even if the new ones are unsynced? y/N ").lower()
+        if evenifunsynced not in yesno:
+            print(f"{Color.YELLOW}Intepreting unknown response as no{Color.OFF}")
+            evenifunsynced = "n"
+    requireexact = input("Require exact artist and title? (Recommended with large folders!!!) Y/n ").lower()
+    if requireexact not in yesno:
+        print(f"{Color.YELLOW}Interpreting unknown response as yes{Color.OFF}")
+        requireexact = "y"
+    
+        
 
 # To suppress CRC check failed warnings - as a pre-existing CRC issue should not affect lyrics
 eyed3.log.setLevel("ERROR")
 with open('current.txt') as current:
     shlong = open("short.txt", 'r')
     short = shlong.readlines()
+    
+    
     for i, file in enumerate(current):
         try: audio_file = eyed3.load(file.strip())
         except:
@@ -133,57 +174,77 @@ with open('current.txt') as current:
             print(str(i+1) + "\tof " + str(total) + f" : {Color.RED}Failed{Color.OFF}  : Unsupported file format              : " +
                   short[i].strip())
             continue
+            
+        '''
+        # Automatically detecting metadata of untagged files
         if audio_file.tag is None:
             audio_file.initTag()
             temp_ind = file.find("-")
-            continue
-        """
-        if len(file) > 0 and temp_ind > 0 and not file.endswith("-"):
-            audio_file.tag.artist = file[0][0:temp_ind]
-            audio_file.tag.title = file[0][temp_ind+1:]
-            print(str(i+1) + "\tof " + str(len(files)) +
-              " : Warning : Artist/Title inferred from file name : " + file[0] + file[1])
-        else:
-            print(str(i+1) + "\tof " + str(len(files)) + " : Failed  : Artist/Title could not be found      : " +
-              file[0] + file[1])
-            continue
-        # didn't adapt this section since this situation doesn't happen to me. also i don't understand the code.
-        """
+            if len(file) > 0 and temp_ind > 0 and not file.endswith("-"):
+                audio_file.tag.artist.set(file[0][0:temp_ind])
+                audio_file.tag.title.set(file[0][temp_ind+1:])
+                audio_file.tag.save()
+                print(str(i+1) + "\tof " + str(len(files)) +
+                  " : Warning : Artist/Title inferred from file name : " + short[i].strip())
+            else:
+                print(str(i+1) + "\tof " + str(len(files)) + " : Failed  : Artist/Title could not be found      : " +
+                  short[i].strip())
+                continue
+        '''    
+                
         existing_lyrics = ""
         for lyric in audio_file.tag.lyrics:
             existing_lyrics += lyric.text
         if len(existing_lyrics) > 0 and overwrite != 'y':
-            print(str(i+1) + "\tof " + str(total) + f" : {Color.YELLOW}Warning{Color.OFF} : File already has lyrics - skipped    : " +
+            print(str(i+1) + "\tof " + str(total) + f" : {Color.YELLOW}Skipped{Color.OFF} : File already has lyrics              : " +
                   short[i].strip())
             continue
         # Note: re.sub... removes anything in brackets - used for "(feat. ...) as this improves search results"
         query = re.sub(r" \[^]+\)", "",
                audio_file.tag.artist + " - " + audio_file.tag.title)
-        site_used = "Lyricsify"
-        ''' i don't actually want to use lyricsify. uncomment if you do.
-        try:
-            lyrics = lyricsify_find_song_lyrics(query)
-        except Exception as e:
-            print("Error getting Lyricsify lyrics for: " + short[i].strip())
-            raise e
-        '''
-        lyrics = None
-        if lyrics is None and genius_access_token is not None:
+               
+
+        if os.getenv("I_WANT_SYNCED_LYRICS") == "True":
+            site_used = "Lyricsify"
+            try:
+                lyrics = lyricsify_find_song_lyrics(query)
+            except Exception as e:
+                print(f"{Color.RED}Error getting Lyricsify lyrics for: " + short[i].strip() + f"{Color.OFF}")
+                raise e
+                
+                
+        if lyrics is None and genius_access_token is not None and ( len(existing_lyrics) == 0 or evenifunsynced == "y" ):
             site_used = "Genius   "
             try:
-                #print(query)
                 lyrics = genius_find_song_lyrics(query, genius_access_token)
             except Exception as e:
-                print("Error getting Genius lyrics for: " + short[i].strip())
+                print(f"{Color.RED}Error getting Genius lyrics for: " + short[i].strip() + f"{Color.OFF}")
                 raise e
+                
+                
+        if b'USLT' in audio_file.tag.frame_set and lyrics is not None :
+            del audio_file.tag.frame_set[b'USLT'] 
+            audio_file.tag.save() # Utterly villainous way to delete the previous lyrics
+            # If this throws an error you should run print(audio_file.tag.frame_set.keys()) instead
+            # USLT is lyrics. b'USLT' means it's stored in bytes instead of as a string
+                
+                
         if lyrics is not None:
             audio_file.tag.lyrics.set(lyrics)
             audio_file.tag.save()
-            print(str(i+1) + "\tof " + str(total) + f" : {Color.GREEN}Success{Color.OFF} : Lyrics from " + site_used + " saved to       : " +
-                  short[i].strip())
+            if inexact == 1:
+                print(str(i+1) + "\tof " + str(total) + f" : {Color.GREEN}Success{Color.OFF} : Lyrics from " + site_used + f" saved to  {Color.YELLOW}(i){Color.OFF}  : " +
+                      short[i].strip())
+            else:
+                print(str(i+1) + "\tof " + str(total) + f" : {Color.GREEN}Success{Color.OFF} : Lyrics from " + site_used + " saved to       : " +
+                      short[i].strip())
+        elif evenifunsynced != "y" and len(existing_lyrics) > 0:
+            print(str(i+1) + "\tof " + str(total) + f" : {Color.YELLOW}Failed{Color.OFF}  : No synced lyrics found               : " +
+              short[i].strip())
         else:
             print(str(i+1) + "\tof " + str(total) + f" : {Color.RED}Failed{Color.OFF}  : Lyrics not found for                 : " +
-                  short[i].strip())
+              short[i].strip())
+              
 os.remove('current.txt')
 os.remove('short.txt')
 # To generate lrc files from AutoLyricize-processed audio files if needed (bash script, requires exiftool):
